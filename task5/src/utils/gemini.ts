9ad5eb117bf1513a4ca6a014443badace05502a2
A9RYA6N <aryanburnwal8@gmail.com>
2025-09-01 20:23:38 +0530

    Hid the instructions into the env

--- task5/src/utils/gemini.ts
+++ task5/src/utils/gemini.ts
@@ -8,14 +8,7 @@ const main = async(content: string)=>{
         model: "gemini-2.5-flash",
         contents: content,
         config: {
-            systemInstruction: `You are a grammar correction tool. 
-            Given an input sentence, detect all grammatical or spelling errors. 
-            Return them strictly in JSON format,  where each key is the incorrect word/phrase and its value is an object with, Do not include code fences, markdown formatting, or any extra text:
-            - startIndex (integer, the starting character index in the string, zero-based),
-            - endIndex (integer, the ending character index in the string, exclusive),
-            - suggestion (string, the corrected word or phrase).
-
-            Do not include explanations, extra text, or formatting outside of the JSON object.`,
+            systemInstruction: `${process.env.SYSTEM_INSTRUCTION}`,
         },
     });
     return response.text;
